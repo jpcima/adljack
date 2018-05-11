79b1c0ebb278ed23eb077613176aa046e98b9fda
JP Cimalando <jpcima@users.noreply.github.com>
2018-05-11 12:44:18 +0200

    add the relase creation script

---
+++ scripts/create-master-release.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -e
+set -x
+
+version=$1
+
+if [ -z "$version" ]; then
+    echo "Package version not given." 1>&2
+    exit 1
+fi
+if [ -e "work" ]; then
+    echo "Delete the work directory first." 1>&2
+    exit 1
+fi
+
+mkdir work
+cd work
+
+git clone .. adljack
+git checkout master
+
+cd adljack
+git submodule--helper list | awk '{ print $4 }' | \
+while read submodule; do
+    git config --file .gitmodules submodule."$submodule".url "../../$submodule"
+done
+git submodule init
+git submodule update
+
+find . -name '.git*' -print0 | xargs -0 rm -rf
+
+cd ..
+mv adljack adljack-"$version"
+
+tar cvf adljack-"$version".tar adljack-"$version"
+gzip -9 adljack-"$version".tar
+
+mkdir adljack-win32
+cd adljack-win32
+i686-w64-mingw32-cmake -DCMAKE_BUILD_TYPE=Release -DPREFER_PDCURSES=ON -DENABLE_VIRTUALMIDI=ON ../adljack-"$version"
+i686-w64-mingw32-cmake --build .
+cpack -G NSIS .
+cpack -G ZIP .
+mv ADLjack-"$version"-win32.exe ../
+mv ADLjack-"$version"-win32.zip ../
+
+cd ..
+
+mkdir adljack-win64
+cd adljack-win64
+x86_64-w64-mingw32-cmake -DCMAKE_BUILD_TYPE=Release -DPREFER_PDCURSES=ON -DENABLE_VIRTUALMIDI=ON ../adljack-"$version"
+x86_64-w64-mingw32-cmake --build .
+cpack -G NSIS .
+cpack -G ZIP .
+mv ADLjack-"$version"-win64.exe ../
+mv ADLjack-"$version"-win64.zip ../
